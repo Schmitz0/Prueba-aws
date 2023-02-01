aa9aeba642873b706d8553f302c38c4af5e86669
Aaron Fraga <aronfraga@gmail.com>
2023-02-01 14:15:08 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+const server = require('./src/app.js');
+const { conn } = require('./src/db.js');
+const { PORT } = process.env;
+
+conn.sync({ force: true }).then(() => {
+  server.listen(PORT, () => {
+    console.log(`Server running...`); 
+  });
+});
