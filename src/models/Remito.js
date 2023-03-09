9ac88054ce759c05b23d336aeba46464dad14c64
pablolospe <yosoypxl@gmail.com>
2023-03-09 10:36:16 -0300

    proveedor en ruta de remito

--- src/models/Remito.js
+++ src/models/Remito.js
@@ -1,21 +1,19 @@
 const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
-   sequelize.define('Remito', {
-  id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true
+  sequelize.define('Remito', {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    fecha: {
+      type: DataTypes.DATEONLY,
+      allowNull: false
+    },
   },
-  fecha: {
-    type: DataTypes.DATEONLY,
-    allowNull: false
-  },
-  total: {
-    type: DataTypes.FLOAT,
-  }
-}, {
-  tableName: 'remitos'
-})
+  {
+    tableName: 'remitos'
+  })
 };
