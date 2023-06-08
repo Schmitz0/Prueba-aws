a0e3d9ad6013c2330fa835db1b15087a69586899
ManuCasanueva <manucasanueva3@gmail.com>
2023-06-08 16:19:41 -0300

    Historial de Stock

--- src/routes/Controllers/movimientoController.js
+++ src/routes/Controllers/movimientoController.js
@@ -1,28 +1,28 @@
-const { Op } = require('sequelize');
+const { Op } = require("sequelize");
 
-const { Router } = require('express');
-const { Insumo } = require('../../db.js');
-const { Receta } = require('../../db.js');
-const { Movimiento } = require('../../db.js');
-const { MovimientoInsumo } = require('../../db.js');
-const {RecetaMovimiento} = require("../../db.js")
-const { updateInsumo } = require('../Controllers/utils.js');
-const moment = require('moment');
+const { Router } = require("express");
+const { Insumo } = require("../../db.js");
+const { Receta } = require("../../db.js");
+const { Movimiento } = require("../../db.js");
+const { MovimientoInsumo } = require("../../db.js");
+const { RecetaMovimiento } = require("../../db.js");
+const { updateInsumo } = require("../Controllers/utils.js");
+const moment = require("moment");
 
 const router = Router();
 
-router.get('/:id', async (req, res) => {
+router.get("/:id", async (req, res) => {
   const { id } = req.params;
   try {
     const movimiento = await Movimiento.findByPk(id, {
       include: [
         {
           model: Insumo,
-          attributes: ['id', 'nombre'],
-          through: { attributes: ['cantidad', 'diferencia'] },
+          attributes: ["id", "nombre"],
+          through: { attributes: ["cantidad", "diferencia"] },
         },
       ],
-      order: [['createdAt', 'DESC']],
+      order: [["createdAt", "DESC"]],
     });
     res.json(movimiento);
   } catch (error) {
@@ -31,9 +31,13 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.get('/', async (req, res) => {
+router.post("/estado", async (req, res) => {
+  const { estado } = req.body;
   try {
     const movimiento = await Movimiento.findAll({
+      where: {
+        estado,
+      },
       include: [
         {
           model: Insumo,
@@ -41,98 +45,181 @@ router.get('/', async (req, res) => {
         {
           model: Receta,
         },
-        
-     
       ],
-      order: [['createdAt', 'DESC']],
+      order: [["createdAt", "DESC"]],
     });
-
 
     res.json(movimiento);
   } catch (error) {
     console.error(error);
-    res.status(500).send('Error al obtener los movimientos');
+    res.status(500).send("Error al obtener los movimientos");
   }
 });
 
-router.post('/', async (req, res) => {
-  const name = req.get('name')
+router.post("/", async (req, res) => {
+  const name = req.get("name");
 
-  const { tipoDeMovimiento, insumos, motivo,tipo } = req.body;
+  const { tipoDeMovimiento, insumos, motivo, tipo } = req.body;
   try {
-    if (tipoDeMovimiento === 'Movimiento de insumo' && tipo === 'suma') {
+    if (tipoDeMovimiento === "Movimiento de insumo" && tipo === "suma") {
       const movimiento = await Movimiento.create({ tipoDeMovimiento, motivo });
       for (const { id, cantidad } of insumos) {
         const insumo = await Insumo.findByPk(id);
         let quantity = insumo.stock;
-        
+        let stockFinal = Number(quantity) + Number(cantidad);
+        let diferencia = cantidad - quantity;
+
         await movimiento.addInsumo(insumo, { through: { cantidad } });
-   
+        await movimiento.addInsumo(insumo, {
+          through: { diferencia: cantidad },
+        });
+        await movimiento.addInsumo(insumo, {
+          through: { stockFinal: stockFinal },
+        });
+        await movimiento.addInsumo(insumo, {
+          through: { stockPrevio: quantity },
+        });
 
         await insumo.update({
           stock: Number(quantity) + Number(cantidad),
         });
 
         await movimiento.update({
-          usuario:name
-        })
+          usuario: name,
+        });
       }
 
       res.json(movimiento);
-
-    } else if(tipoDeMovimiento === 'Movimiento de insumo' && tipo === 'resta') {  
+    } else if (
+      tipoDeMovimiento === "Movimiento de insumo" &&
+      tipo === "resta"
+    ) {
       const movimiento = await Movimiento.create({ tipoDeMovimiento, motivo });
-    for (const { id, cantidad } of insumos) {
-      const insumo = await Insumo.findByPk(id);
-      let quantity = insumo.stock;
-      await movimiento.addInsumo(insumo, { through: { cantidad } });
+      for (const { id, cantidad } of insumos) {
+        const insumo = await Insumo.findByPk(id);
+        let quantity = insumo.stock;
+        let stockFinal = Number(quantity) - Number(cantidad);
+        let diferencia = cantidad - quantity;
 
-      await insumo.update({
-        stock: Number(quantity) - Number(cantidad),
-      });
-      
+        await movimiento.addInsumo(insumo, { through: { cantidad } });
+        await movimiento.addInsumo(insumo, {
+          through: { diferencia: -cantidad },
+        });
+        await movimiento.addInsumo(insumo, {
+          through: { stockFinal: stockFinal },
+        });
+        await movimiento.addInsumo(insumo, {
+          through: { stockPrevio: quantity },
+        });
 
-      await movimiento.update({
-        usuario:name
-      })
+        await insumo.update({
+          stock: Number(quantity) - Number(cantidad),
+        });
 
-    }
+        await movimiento.update({
+          usuario: name,
+        });
+      }
 
-    res.json(movimiento);
+      res.json(movimiento);
     } else {
       const movimiento = await Movimiento.create({ tipoDeMovimiento, motivo });
       for (const { id, cantidad } of insumos) {
         const insumo = await Insumo.findByPk(id);
         let quantity = insumo.stock;
+        let stockFinal = quantity - cantidad;
         let diferencia = cantidad - quantity;
 
         await movimiento.addInsumo(insumo, { through: { cantidad } });
-        await movimiento.addInsumo(insumo, { through: { diferencia } });
-
-        await insumo.update({
-          stock: Number(quantity) + Number(diferencia),
+        await movimiento.addInsumo(insumo, {
+          through: { diferencia: diferencia },
+        });
+        await movimiento.addInsumo(insumo, {
+          through: { stockFinal: cantidad },
+        });
+        await movimiento.addInsumo(insumo, {
+          through: { stockPrevio: quantity },
         });
 
+        // await insumo.update({
+        //   stock: Number(quantity) + Number(stockFinal),
+        // });
+
         await movimiento.update({
-          usuario:name
-        })
-        
+          usuario: name,
+          estado: false,
+        });
       }
-        
+
       res.json(movimiento);
     }
   } catch (error) {
     console.error(error);
-    res.status(500).send('Error al crear el movimiento');
+    res.status(500).send("Error al crear el movimiento");
+  }
+});
+
+router.post("/historial/:id", async (req, res) => {
+  const name = req.get("name");
+  const { id } = req.params; // Obtén el ID del movimiento desde req.params
+  // const { tipoDeMovimiento, insumos} = req.body;
+
+  try {
+    
+      const movimiento = await Movimiento.findByPk(id,{
+        include: [
+          {
+            model: Insumo,
+          },
+        ]
+      });
+  
+     
+      for (let i = 0; i < movimiento.Insumos?.length; i++) {
+         const idInsumo = movimiento.Insumos[i].MovimientoInsumo.InsumoId
+        const insumo = await Insumo.findByPk(idInsumo);
+        
+        const quantity = insumo.stock;
+      console.log(quantity);
+        const stockFinal = movimiento.Insumos[i].MovimientoInsumo.stockFinal
+
+        // const stockFinal = quantity - cantidad;
+        // const diferencia = cantidad - quantity;
+
+        // await movimiento.addInsumo(insumo, { through: { cantidad } });
+        // await movimiento.addInsumo(insumo, { through: { diferencia: diferencia } });
+        // await movimiento.addInsumo(insumo, { through: { stockFinal: cantidad } });
+        // await movimiento.addInsumo(insumo, { through: { stockPrevio: quantity } });
+
+        await insumo.update({
+          stock: Number(stockFinal),
+        });
+        
+        await movimiento.update({
+          usuario: name,
+          estado: true,
+        });
+      }
+
+      res.json(movimiento);
+      }
+
+   catch (error) {
+    console.error(error);
+    res.status(500).send("Error al crear el movimiento");
   }
 });
 
 router.post("/filters", async (req, res) => {
   const { filters } = req.body;
 
-   try {
-    const fecha1 = filters.fechaMin ? moment(filters.fechaMin, "DD/MM/YYYY").format("YYYY-MM-DD") : null;
-    const fecha2 = filters.fechaMax ? moment(filters.fechaMax, "DD/MM/YYYY").format("YYYY-MM-DD") : null;
+  try {
+    const fecha1 = filters.fechaMin
+      ? moment(filters.fechaMin, "DD/MM/YYYY").format("YYYY-MM-DD")
+      : null;
+    const fecha2 = filters.fechaMax
+      ? moment(filters.fechaMax, "DD/MM/YYYY").format("YYYY-MM-DD")
+      : null;
     const insumoNombre = filters.insumo ? filters.insumo : null;
 
     const whereClause = {};
@@ -167,23 +254,25 @@ router.post("/filters", async (req, res) => {
       include: [
         {
           model: Insumo,
-          attributes: ['id', 'nombre', 'precio'],
-          through: { attributes: ['cantidad'] },
+          attributes: ["id", "nombre", "precio"],
+          through: { attributes: ["cantidad"] },
           where: {
-            nombre: insumoNombre ? {[Op.like]: insumoNombre} : {[Op.not]: "cloudinary"}
-            },
+            nombre: insumoNombre
+              ? { [Op.like]: insumoNombre }
+              : { [Op.not]: "cloudinary" },
           },
+        },
         {
           model: MovimientoInsumo,
-          attributes: ['cantidad'],
+          attributes: ["cantidad"],
           include: {
             model: Insumo,
-            attributes: ['id', 'nombre', 'precio'],
+            attributes: ["id", "nombre", "precio"],
           },
         },
       ],
-      order: [['createdAt', 'DESC']],
-      limit: 10
+      order: [["createdAt", "DESC"]],
+      limit: 10,
     });
 
     res.json(movimientos);
@@ -193,86 +282,92 @@ router.post("/filters", async (req, res) => {
   }
 });
 
-router.post('/:id', async (req, res) => {
-
-  const name = req.get('name')
+router.post("/:id", async (req, res) => {
+  const name = req.get("name");
   const { tipoDeMovimiento, motivo, cantidadProducida } = req.body;
   const { id } = req.params;
 
   try {
-
-    if (tipoDeMovimiento === 'Receta') {
+    if (tipoDeMovimiento === "Receta") {
       const movimiento = await Movimiento.create({ tipoDeMovimiento, motivo });
+
       await movimiento.update({ cantidadProducida });
-      await movimiento.update({usuario:name})
+      await movimiento.update({ usuario: name });
       const receta = await Receta.findByPk(id, {
         include: [
           {
             model: Insumo,
-            through: { attributes: ['cantidad', 'costo', 'costoPorBotella'] },
+            through: { attributes: ["cantidad", "costo", "costoPorBotella"] },
           },
         ],
       });
-  
+
       await movimiento.addReceta(receta, { through: { cantidadProducida } });
 
-        const aux = await Promise.all(receta.Insumos?.map(async (e) => {
-        let insumo = await (e.id);
-        let cantidadUsada = await (e.InsumoReceta.cantidad);
-        const insumoACambiar = await Insumo.findByPk(insumo);
-        const tot = cantidadUsada*cantidadProducida;
-        await insumoACambiar.update({
-          stock: Number(insumoACambiar.stock)-Number(tot),
-        });
-        await movimiento.update({motivo:receta.name})
-          await movimiento.addInsumo(insumo, { through: { cantidad:tot } });
-          
-      }));
+      const aux = await Promise.all(
+        receta.Insumos?.map(async (e) => {
+          // const stockPrevio =  await movimiento.addReceta(receta, { through: { cantidadProducida } });
+
+          let insumo = await e.id;
+          let stockPrevio = await e.stock;
+          let cantidadUsada = await e.InsumoReceta.cantidad;
+          const insumoACambiar = await Insumo.findByPk(insumo);
+          const tot = cantidadUsada * cantidadProducida;
+          const stockFinal = Number(stockPrevio) - Number(tot);
+          await insumoACambiar.update({
+            stock: Number(insumoACambiar.stock) - Number(tot),
+          });
+          await movimiento.update({ motivo: receta.name });
+          await movimiento.addInsumo(insumo, { through: { cantidad: tot } });
+          await movimiento.addInsumo(insumo, { through: { diferencia: tot } });
+          await movimiento.addInsumo(insumo, {
+            through: { stockPrevio: stockPrevio },
+          });
+          await movimiento.addInsumo(insumo, {
+            through: { stockFinal: stockFinal },
+          });
+        })
+      );
 
       res.json(movimiento);
     }
   } catch (error) {
     console.error(error);
-    res.status(500).send('Error al crear el movimiento');
+    res.status(500).send("Error al crear el movimiento");
   }
 });
 
-
-
-router.delete('/:id', async (req, res) => {
+router.delete("/:id", async (req, res) => {
   try {
     const { id } = req.params;
     const movABorrar = await Movimiento.findByPk(id, {
       include: [
         {
           model: Insumo,
-          attributes: ['stock'],
+          attributes: ["stock"],
         },
       ],
     });
 
-    if (!movABorrar) throw new Error('El ID del movimiento no fue encontrado');
+    if (!movABorrar) throw new Error("El ID del movimiento no fue encontrado");
 
     if (
-      movABorrar.tipoDeMovimiento === 'Movimiento de insumo' ||
-      movABorrar.tipoDeMovimiento === 'Receta'
+      movABorrar.tipoDeMovimiento === "Movimiento de insumo" ||
+      movABorrar.tipoDeMovimiento === "Receta"
     ) {
       const ins = movABorrar.Insumos;
- 
 
       for (let i = 0; i < ins.length; i++) {
         let insId = ins[i].MovimientoInsumo.InsumoId;
         let insQuantity = Number(ins[i].MovimientoInsumo.cantidad);
-        
+
         const insumo = await Insumo.findByPk(insId);
         let quantity = Number(insumo.stock);
 
         await insumo.update({
           stock: Number(quantity) + Number(insQuantity),
         });
-
       }
-
 
       movABorrar.destroy();
       res.status(200).send(movABorrar);
@@ -297,8 +392,5 @@ router.delete('/:id', async (req, res) => {
     res.status(400).send(error.message);
   }
 });
-
-
-
 
 module.exports = router;
