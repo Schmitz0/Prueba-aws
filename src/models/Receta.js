ea23037f58d2eced70b92efd04ab2e1ce2fa6c7c
pablolospe <yosoypxl@gmail.com>
2023-06-09 11:40:03 -0300

    pooool

--- src/models/Receta.js
+++ src/models/Receta.js
@@ -5,7 +5,7 @@ module.exports = (sequelize) => {
         name: {
             type: DataTypes.STRING(50),
             allowNull: false,
-            unique:true,
+            // unique:true,
         },
         costoPorReceta: {
             type: DataTypes.FLOAT,
